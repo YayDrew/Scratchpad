1dae3e00443722b207d52b5b0c251c68e0a35d65
YayDrew <AndrewTech63@gmail.com>
2025-04-12 16:10:24 -0400

    chatgpt improvement

--- Dataaccess.py
+++ Dataaccess.py
@@ -1,13 +1,12 @@
-'''here is where we use the questionFile variable
- the below could be rewritten as f = open(questionFile)
- it could also be rewritten as lines = open(questionFile).readlines()# the previous line would require commands like TRY and include error handling so we use WITH
- to take care of common errors.'''
 def getlines(usefile, questionFile):
     if usefile: 
+        # Open the file safely using 'with' so it's closed properly, even if there's an error
         with open(questionFile) as f:
             lines = f.readlines()
-            """print(type(lines))
-            print(type('Bob'))"""
     else: 
-        lines = [ 'Who invented the backwards worm?~Owen', 'Who was a powerful landscaper?~Drew']
+        # Use a hardcoded fallback list of questions
+        lines = [
+            'Who invented the backwards worm?~Owen',
+            'Who was a powerful landscaper?~Drew'
+        ]
     return lines
